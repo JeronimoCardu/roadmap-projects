e6383c60048c0d7f80bc3d4dc3dec851b8e5647f
Jeronimo Cardu <jeronimocardu29@gmail.com>
2024-11-20 18:59:49 -0300

    Age Calculator upgraded finished

--- Age Calculator/main.js
+++ Age Calculator/main.js
@@ -19,34 +19,53 @@ function validInput( date ){
 }
 
 function calculateAge( date ){
+    let msg;
     let dateObj = {
         year: date.slice(6),
         month: date.slice(3, 5),
         day: date.slice(0, 2)
     }
     
-    const DateTime = luxon.DateTime;
     let nowTime = new Date();
-    let timeTo = DateTime.now(nowTime).setZone("system").minus({
-        year: dateObj.year,
-        month: dateObj.month,
-        day: dateObj.day    
-    }).endOf("day").toISO();
-    
-    let timeLived = {
-        years: parseInt(timeTo.slice(0,4)),
-        months: parseInt(timeTo.slice(5, 7)),
-        days: parseInt(timeTo.slice(8, 10))
+    let allMonths = [
+        [1, 31],
+        [2, 29],
+        [3, 30],
+        [4, 31],
+        [5, 30],
+        [6, 31],
+        [7, 30],
+        [8, 31],
+        [9, 30],
+        [10, 31],
+        [11, 30],
+        [12, 31]
+    ]
+    if(dateObj.year > nowTime.getFullYear() || allMonths[dateObj.month-1][1] < dateObj.day || dateObj.month > 12){
+        msg = "Your birth isn't correct";
+    } else{
+        const DateTime = luxon.DateTime;
+        let timeTo = DateTime.now(nowTime).setZone("system").minus({
+            year: dateObj.year,
+            month: dateObj.month,
+            day: dateObj.day    
+        }).endOf("day").toISO();
+        
+        let timeLived = {
+            years: parseInt(timeTo.slice(0,4)),
+            months: parseInt(timeTo.slice(5, 7)),
+            days: parseInt(timeTo.slice(8, 10))
+        }
+        msg =   `
+                Your are 
+                <strong>
+                    ${timeLived.years} years ${timeLived.months} months and ${timeLived.days} days
+                </strong>
+                old
+                `
     }
-    document
-        .getElementById("result")
-        .innerHTML = `
-            Your are 
-            <strong>
-                ${timeLived.years} years ${timeLived.months} months and ${timeLived.days} days
-            </strong>
-            old
-        `
+    
+    return msg
 }
 
 
@@ -64,5 +83,9 @@ inputDate.addEventListener("input", () => {
 
 const btnToCalculate = document.getElementById("btn-calculate");
 if(!btnToCalculate.getAttribute("Disabled")){
-    btnToCalculate.addEventListener("click", () => calculateAge(inputDate.value))
+    btnToCalculate.addEventListener("click", () => {
+        document
+            .getElementById("result")
+            .innerHTML = calculateAge(inputDate.value)
+    })
 }
